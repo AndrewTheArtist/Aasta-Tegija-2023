ba7d305594b6503bccb89636c3da5cf9de328d61
Henry-Gerret Grüning <henryg.gruning@gmail.com>
2023-03-07 15:04:04 +0200

    As a User I can login so I can have admin privilieges

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import './App.css';
 import Dashboard from '../Dashboard/Dashboard';
@@ -28,14 +28,10 @@ function App() {
     <div className="wrapper">
       <h1>Application</h1>
       <BrowserRouter>
-        <Switch>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/preferences">
-            <Preferences />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/preferences" element={<Preferences />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
